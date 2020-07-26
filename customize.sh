6fedc738de636c9eb81badb2412fcd84f0509c66
yh1272000 <58769308+yh1272000@users.noreply.github.com>
2020-07-26 09:33:23 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,4 +6,5 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.100.81/g' openwrt/package/base-files/files/bin/config_generate
